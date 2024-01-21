6a0353d5e16e765a8b04457d352993738c9a1a28
sunshj <sunshj317@qq.com>
2024-01-21 12:06:26 +0800

    chore: 删除无用的类型

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -3,17 +3,11 @@ import { Server, Socket } from 'socket.io'
 declare global {
   type RoomSide = 'client' | 'admin'
   interface ServerToClientEvents {
-    Verify: (msg: string) => void
-    VerifyPassed: (msg: string) => void
-    VerifyFailed: (msg: string) => void
     Message: (msg: string) => void
-    NewOrderReceived: ({ msg, orderId }: { msg: string; orderId: number }) => void
+    NewOrderReceived: (data: { msg: string; orderId: number }) => void
   }
 
   interface ClientToServerEvents {
-    Verify: ({ clientSide }: { clientSide: boolean }) => void
-    VerifyPassed: (msg: string) => void
-    VerifyFailed: (msg: string) => void
     NewOrder: (orderId: number) => void
   }
 
